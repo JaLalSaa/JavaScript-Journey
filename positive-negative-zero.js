0b90ff4a3ec2f51a05ee11fcd5dbf064b51feeb5
JaLalSaa <jalalsadeq724@gmail.com>
2025-08-18 17:47:02 +0100

    Add check for non-numeric values using typeof and isNaN

--- positive-negative-zero.js
+++ positive-negative-zero.js
@@ -1,23 +1,28 @@
+// -------------------------------
 // Using if…else statements
-let Num = 10;
+// -------------------------------
+let number = 15;
 
-// Check if the number is positive, negative, or zero
-if (Num > 0) {
-    console.log(`${Num} is a Positive Number`);  // Number is greater than 0
-} else if (Num < 0) {
-    console.log(`${Num} is a Negative Number`);  // Number is less than 0
+if (typeof number !== "number" || isNaN(number)) {
+    console.log(`${number} is not a number`);
+} else if (number > 0) {
+    console.log(`${number} is a Positive Number`);
+} else if (number < 0) {
+    console.log(`${number} is a Negative Number`);
 } else {
-    console.log(`The number is zero`);              // Number is exactly 0
+    console.log(`${number} is Zero`);
 }
 
 
+// -------------------------------
 // Using ternary operator
-let x = -15;
-// Ternary operator works like a shortcut for if…else
-// Syntax: condition ? value_if_true : value_if_false
-let result =
-    x > 0 ? `${x} is a positive number`  
+// -------------------------------
+let x = 45;
+
+let result = 
+    typeof x !== "number" || isNaN(x) ? `${x} is not a number`
+    : x > 0 ? `${x} is a positive number`
     : x < 0 ? `${x} is a negative number`
-    : `The number is zero`;                    
+    : `Your number is zero`;
 
 console.log(result);
